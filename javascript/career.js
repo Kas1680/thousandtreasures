ba2f0fcba7b08cb28795be918ab0110f4088e98b
Kas1680 <kas1914080@gmail.com>
2023-08-30 08:15:35 -0700

    Another update

--- javascript/career.js
+++ javascript/career.js
@@ -1,6 +1,6 @@
 const boxData = [
   { link: "../index.html", text: "Go Back" },
-  { link: "https://clients.mindbodyonline.com/consumermyinfo?studioid=5732854&tg=&vt=&lvl=&stype=&view=&trn=0&page=&catid=&prodid=&date=8%2f28%2f2023&classid=0&prodGroupId=&sSU=&optForwardingLink=&qParam=&justloggedin=&nLgIn=&pMode=0&loc=1#", text: "MindBody" },
+
   { link: "https://leetcode.com/studyplan/top-interview-150/", text: "LeetCode" }
   // { link: "", text: "" }
 ];
--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,5 @@
 const boxData = [
-    // { link: "https://chat.openai.com/", text: "ChatGPT" },
-    // { link: "https://simplepdf.eu/editor", text: "PDF Editor" },
-    // { link: "https://blank.page/", text: "Blank Page" },
-    // { link: "https://mail.google.com/", text: "GMail" },
-    // { link: "https://github.com/", text: "Github" },
-    // { link: "https://www.sfu.ca/sfumail.html", text: "SFU Mail" },
-    // { link: "https://www.toptal.com/project-managers/tomato-timer", text: "Pomodoro" },
-    // { link: "https://clients.mindbodyonline.com/consumermyinfo?studioid=5732854&tg=&vt=&lvl=&stype=&view=&trn=0&page=&catid=&prodid=&date=8%2f28%2f2023&classid=0&prodGroupId=&sSU=&optForwardingLink=&qParam=&justloggedin=&nLgIn=&pMode=0&loc=1#", text: "MindBody" },
-    // { link: "https://leetcode.com/studyplan/top-interview-150/", text: "LeetCode" },
-    // { link: "src/sch.jpg", text: "mySchedule" }
+
     // { link: "", text: "" }
     { link: "../html/gadget.html", text: "Gadgets" },
     { link: "../html/school.html", text: "School" },
@@ -30,9 +21,8 @@ const boxData = [
     box.appendChild(boxContent);
     boxContainer.appendChild(box);
   });
+// const baseUrl = window.location.href.includes('5500') ?
+//    'https://kas1680.github.io/thousandtreasures.io/' : '' ;
 
-const baseUrl = window.location.href.includes('5500') ?
-  '' : 'https://kas1680.github.io/thousandtreasures.io/';
 
-document.write('<base href="' + baseUrl + '">');
-  
+
